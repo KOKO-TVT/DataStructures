4aa886df2b092ebf5e0bdc64a7f87262b3c52d5c
CaiLang <112542016+KOKO-TVT@users.noreply.github.com>
2023-05-08 21:50:38 +0800

    5.8

--- DataStructures/LinkList.cpp
+++ DataStructures/LinkList.cpp
@@ -8,33 +8,74 @@ struct node
 };
 node* head;	//头节点初始化
 
-void insert(int x)	//节点插入
+void insert(node** PointerHead,int x)	//在头部节点插入
 {
 	node* temp = new node();
 	temp->data = x;
-		temp->next = head;
-		head = temp;
-		
+	temp->next = nullptr;
+	if(*PointerHead != nullptr)
+	{
+		temp->next = *PointerHead;
+	}
+	head = temp;
+
+}
+void insert1(int data, int pos)		//在任意位置插入节点
+{
+	node* temp1 = new node();		//插入位置为1情况
+	temp1->data = data;
+	temp1->next = nullptr;
+	if(pos==1)						//链表为空也适用
+	{
+		temp1->next = head;
+		head = temp1;
+		return;
 	}
 
-void print()
+	//当插入的位置大于1时
+	node* temp2 = head;			//创建得该节点为一个遍历至特定位置得媒介
+	for(int i=0;i<pos-2;i++)
+	{
+		temp2 = temp2->next;
+	}
+	if(temp2=nullptr)
+	temp1->next = temp2->next;
+	temp2->next = temp1;
+}
+void print(node* head)
 {
-	
+	cout << "你所期望得链表为：\n";
+	while (head!= nullptr)
+	{
+		cout << head->data<<"\t";
+		head = head->next;
+	}
+	cout << "\n";
 }
 
 int main()
 {
 	head = nullptr;
-	cout << "你想要多少个数据?\n";
-	int n;
-	cin >> n;
-	for(int i:n)
-	{
-		cout << "请输入你想要填入的信息\n";
-		int x;
-		cin >> x;
-		insert(x);
-		print();
+															//对从头部开始插入函数得测试
+															/*	cout << "你想要多少个数据?\n";
+																int n,x;
+																cin >> n;
+																for(int i=0;i<n;i++)
+																{
+																	cout << "请输入你想要填入的信息\n";
+																	cin >> x;
+																	insert(&head,x);
+																	print(head);
 
-	}
+																}
+															*/
+
+	//对任意位置插入得函数进行测试
+	insert1(2, 1);
+	insert1(3, 2);
+	insert1(4, 1);
+	insert1(5, 2);
+	insert1(55, 6);
+	print(head);
+
 }
