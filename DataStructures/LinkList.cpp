d3e0a3905dc1ff9c6670b67d7d806d5da02d1021
CaiLang <112542016+KOKO-TVT@users.noreply.github.com>
2023-05-08 12:02:35 +0800

    5.8

---
+++ DataStructures/LinkList.cpp
@@ -0,0 +1,47 @@
+//链表
+#include <iostream>
+using namespace std;
+struct node
+{
+	int data;
+	node* next;
+};
+node* head;	//头节点初始化
+
+void insert(int x)	//节点插入
+{
+	node* temp = new node();
+	temp->data = x;
+	temp->next = nullptr;
+	if(head!=nullptr)
+	{
+		temp->next = head;
+		head = temp;
+	}
+	else
+	{
+		
+	}
+}
+
+void print()
+{
+	
+}
+
+int main()
+{
+	head = nullptr;
+	cout << "你想要多少个数据?\n";
+	int n;
+	cin >> n;
+	for(int i:n)
+	{
+		cout << "请输入你想要填入的信息\n";
+		int x;
+		cin >> x;
+		insert(x);
+		print();
+
+	}
+}
