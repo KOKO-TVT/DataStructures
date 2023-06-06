33244fbeaa3bca30d11524fa9d338e43480ef59d
TianPengCheng <1786729760@qq.com>
2023-06-06 19:00:51 +0800

    6.6

--- DataStructures/BPTree.cpp
+++ DataStructures/BPTree.cpp
@@ -61,9 +61,90 @@ void BTrees::Insert(int x)
 	}
 }
 
-void BTrees::Split(int, node* temp1, node* temp2)
+void BTrees::Split(int x, node* parent, node* temp)
 {
+	node* LLeaf = new node();
+	node* RLeaf = new node();
+	InsertVal(x, temp);
+	LLeaf->IsLeaf = RLeaf->IsLeaf = true;	//分裂出的左右叶子节点
+	//分配关键字
+	LLeaf->size = (max + 1) / 2;
+	RLeaf->size = (max + 1) - (max + 1) / 2;
 
+	for (int i = 0; i < max + 1; i++)		//分配链表
+		LLeaf->ptr[i] = temp->ptr[i];
+	LLeaf->ptr[LLeaf->size] = RLeaf;
+	RLeaf->ptr[RLeaf->size] = LLeaf->ptr[max];
+	LLeaf->ptr[max] = nullptr;					//叶子节点链表继承完毕
 
+	//数据继承
+	for(int i=0;i<LLeaf->size;i++)
+	{
+		LLeaf->key[i] = temp->key[i];
+	}
+	for(int i=0,j=LLeaf->size;i<RLeaf->size;i++,j++)
+	{
+		RLeaf->key[i] = temp->key[j];
+	}
+
+	if(temp == root)			//当插入数据的结点为根结点时，即数据中要重新推出一个结点为根结点
+	{
+		node* NewRoot = new node();
+		NewRoot->key[0] = RLeaf->key[0];
+		NewRoot->ptr[0] = LLeaf;
+		NewRoot->ptr[1] = RLeaf;
+		NewRoot->IsLeaf = false;
+		NewRoot->size = 1;
+		LLeaf->parent = RLeaf->parent = root = NewRoot;
+	}
+
+	else
+	{
+		InsertInternal(RLeaf->key[0], parent, LLeaf, RLeaf);	//插入关键字的结点不为根节点时
+	}
+}
+
+void BTrees::InsertInternal(int x, node* temp, node* LLeaf, node* RLeaf)		//向上提的关键字结点分配
+{
+	if(temp->size<max)
+	{
+		int i = InsertVal(x, temp);
+		for(int j = temp->size;j>i+1;i--)		
+		{
+			temp->ptr[j] = temp->ptr[j - 1];
+		}
+		temp->ptr[i] = LLeaf;
+		temp->ptr[i + 1] = RLeaf;
+	}
+	else				//提上去的结点超过理论容纳，继续往上提
+	{
+		node* NewLchild = new node();				//父节点容量已满重新进行分裂并分配新的左右子节点
+		node* NewRchild = new node();
+		node* virtualPtr[max + 2];					//容量已经超了，所以肯定为4个子结点
+		for(int i = 0;i<max+1;i++)
+		{
+			virtualPtr[i] = temp->ptr[i];
+		}
+		int i = InsertVal(x, temp);
+		for(int j = max+2;j>i+1;j--)
+		{
+			virtualPtr[j] = virtualPtr[j - 1];
+		}
+		virtualPtr[i] = LLeaf;			//暂存数据
+		virtualPtr[i + 1] = RLeaf;
+		NewLchild->IsLeaf = NewRchild->IsLeaf = false;
+		//处理新分配的左右结点数据
+		NewLchild->size = (max + 1) / 2;
+		NewRchild->size = max - (max + 1) / 2;
+
+		for(int i = 0;i<NewLchild->size;i++)
+		{
+			NewLchild->key[i] = temp->key[i];
+		}
+		for(int i = 0,j = NewLchild->size+1;i<NewRchild->size;i++,j++)
+		{
+			
+		}
+	}
 }
 
--- DataStructures/BPTree.h
+++ DataStructures/BPTree.h
@@ -6,10 +6,10 @@ using namespace std;
 class node
 {
 	bool IsLeaf;
-	int* key;	//关键字
-	node** ptr;	//结点
-	node	*parent = nullptr;
-	int size;	//关键字尺寸
+	int* key;					//关键字
+	node** ptr;					//结点
+	node	*parent;	
+	int size;					//结点当前大小
 	friend class BTrees;		//声明友元	不然btree中的函数无法访问变量
 public:
 	node():key(new int[max+1]),parent(nullptr),ptr(new node*[max+1]) {}
@@ -20,8 +20,8 @@ class BTrees
 	node* root = nullptr;
 	void InsertInternal(int, node*, node*, node*);			//节点插入
 	void Split(int, node*, node*);					//分裂节点
-	int InsertVal(int, node*);						//寻找节点的插入位置
+	int InsertVal(int, node*);						//进入结点，忽略大小限制，寻找节点的插入位置
 public:
-	void Insert(int x);
+	void Insert(int x);								//寻找关键字应该插入的叶子结点位置
 	void Print();									//打印结果
 };
